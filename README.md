140f0ffbff76a1106ae4641664092e4e6b2daabf
Oliver Charles <ollie@ocharles.org.uk>
2018-08-11 09:37:25 +0100

    MVP

---
+++ README.md
@@ -0,0 +1,40 @@
+# hlint-source-plugin
+
+This repository contains a proof-of-concept for a source plugin that calls
+HLint. This means that at compilation time, HLint is called, rather than having
+HLint being a separate extra compilation step.
+
+## Example
+
+There is an example project in [`example`](./example), which demonstrates many
+coding practices that can be detected by HLint. When you compile this module,
+you'll see HLint warnings:
+
+```sh
+$ cabal new-build all
+Build profile: -w ghc-8.6.1.20180716 -O1
+In order, the following will be built (use -v for more details):
+ - example-0.1.0.0 (exe:example) (dependency rebuilt)
+Preprocessing executable 'example' for example-0.1.0.0..
+Building executable 'example' for example-0.1.0.0..
+[1 of 1] Compiling Main             ( Main.hs, /home/ollie/work/hlint-source-plugin/dist-newstyle/build/x86_64-linux/ghc-8.6.1.20180716/example-0.1.0.0/x/example/build/example/example-tmp/Main.o ) [Plugin forced recompilation]
+
+Main.hs:4:8: warning: Redundant do
+  |
+4 | main = do
+  |        ^^...
+```
+
+Here we see that HLint has detected a redundant do, and this has been turned
+into a GHC warning.
+
+## Future Work
+
+Currently this plugin simply hooks into the parse stage and calls HLint with a
+file path. This means HLint will re-parse all source code. The next logical step
+is to use the actual parse tree, as given to us by GHC, and HLint that. This
+means that HLint can lose the special logic to run CPP, along with the hacky
+handling of fixity resolution (we get that done correctly by GHC's renaming
+phase).
+
+This isn't hard work, just fairly tedious. Contributions welcome!
---
+++ plugin/src/HLint.hs
@@ -0,0 +1,72 @@
+{-# language RecordWildCards #-}
+
+module HLint ( plugin ) where
+
+-- base
+import Control.Monad.IO.Class ( liftIO )
+
+-- ghc
+import qualified Bag
+import qualified DynFlags
+import qualified ErrUtils
+import qualified FastString
+import qualified GhcMonad
+import qualified HscTypes
+import qualified Outputable
+import qualified Plugins
+import qualified SrcLoc
+
+-- haskell-src-exts
+import qualified Language.Haskell.Exts.SrcLoc as HSE
+
+-- hlint
+import qualified Language.Haskell.HLint3 as HLint
+
+
+plugin :: Plugins.Plugin
+plugin =
+  Plugins.defaultPlugin
+    { Plugins.parsedResultAction =
+        \_cliArgs modSummary parsedModule ->
+          parsedModule <$ hlintPlugin modSummary
+    }
+
+
+hlintPlugin :: HscTypes.ModSummary -> HscTypes.Hsc ()
+hlintPlugin modSummary = do
+  dynFlags <-
+    DynFlags.getDynFlags
+
+  ideas <-
+    liftIO ( HLint.hlint [ "--quiet", HscTypes.ms_hspp_file modSummary ] )
+
+  liftIO
+    ( HscTypes.printOrThrowWarnings
+        dynFlags
+        ( Bag.listToBag ( map ( ideaToErrMsg dynFlags ) ideas ) )
+    )
+
+  where
+
+    ideaToErrMsg dynFlags idea =
+      ( case HLint.ideaSeverity idea of
+          HLint.Error ->
+            ErrUtils.mkPlainErrMsg
+
+          _ ->
+            ErrUtils.mkPlainWarnMsg
+      )
+        dynFlags
+        ( let
+            HSE.SrcSpan{..} =
+              HLint.ideaSpan idea
+
+            fileName =
+              FastString.fsLit srcSpanFilename
+
+          in
+          SrcLoc.mkSrcSpan
+            ( SrcLoc.mkSrcLoc fileName srcSpanStartLine srcSpanStartColumn )
+            ( SrcLoc.mkSrcLoc fileName srcSpanEndLine srcSpanEndColumn )
+        )
+        ( Outputable.text ( HLint.ideaHint idea ) )
